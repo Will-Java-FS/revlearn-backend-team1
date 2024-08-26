cbb24355c77268b3759bd7a5481822e01eedd020
Daniel Thompson <DThompson124@my.gcu.edu>
2024-08-26 13:13:00 -0700

    course-entity progress (rebase test)

---
+++ src/main/java/com/revlearn/team1/model/TransactionModel.java
@@ -0,0 +1,29 @@
+package com.revlearn.team1.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import com.revlearn.team1.model.UserModel;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class TransactionModel
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id", referencedColumnName = "to_id")
+    User to_user;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id", referencedColumnName = "from_id")
+    User from_user;
+
+    float price;
+    String Description;
+}
---
+++ src/main/java/com/revlearn/team1/service/TransactionService.java
@@ -0,0 +1,9 @@
+package com.revlearn.team1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TransactionService
+{
+
+}
