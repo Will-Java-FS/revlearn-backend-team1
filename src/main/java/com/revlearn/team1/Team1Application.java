4ff607a95207e9e16ad6b407384fee500768432f
Trey Crossley <treycrossley@gmail.com>
2024-08-26 12:34:24 -0700

    spring init

---
+++ src/main/java/com/revlearn/team1/Team1Application.java
@@ -0,0 +1,13 @@
+package com.revlearn.team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team1Application.class, args);
+	}
+
+}
