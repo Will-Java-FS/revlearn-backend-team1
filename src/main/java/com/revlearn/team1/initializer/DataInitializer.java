fb2d5a5e6679ea6e839ffcaf69f95d9fb5aae1c5
Trey Crossley <treycrossley@gmail.com>
2024-08-28 01:09:30 -0700

    added Dummy courses

--- src/main/java/com/revlearn/team1/initializer/DataInitializer.java
+++ src/main/java/com/revlearn/team1/initializer/DataInitializer.java
@@ -1,5 +1,8 @@
 package com.revlearn.team1.initializer;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,9 +16,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import com.revlearn.team1.dto.CourseDTO;
 import com.revlearn.team1.dto.TransactionDTO;
+import com.revlearn.team1.enums.AttendanceMethod;
 import com.revlearn.team1.model.User;
 import com.revlearn.team1.repository.UserRepository;
+import com.revlearn.team1.service.CourseService;
+import com.revlearn.team1.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,6 +32,8 @@ public class DataInitializer implements ApplicationRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(DataInitializer.class);
     private final UserRepository userRepository;
+    private final UserService userService;
+    private final CourseService courseService;
 
     @Value("${SPRING_API_URL}")
     private String baseUrl;
@@ -35,6 +44,7 @@ public class DataInitializer implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         logger.info("Initializing data...");
         createDefaultUsers();
+        createDummyCourses();
         createDummyTransactions();
     }
 
@@ -46,6 +56,44 @@ public class DataInitializer implements ApplicationRunner {
         userRepository.save(user2);
 
         logger.info("Default users created.");
+    }
+
+    private void createDummyCourses() {
+        // Fetch existing users to use as educators and students
+        User educator = userService.findById(1L).orElseThrow();
+        User student = userService.findById(2L).orElseThrow();
+
+        CourseDTO[] courses = {
+                new CourseDTO(
+                        null, // id is optional and will be auto-generated
+                        "Java Programming",
+                        "An advanced course on Java programming.",
+                        educator.getId(), // institutionId
+                        LocalDate.of(2024, 9, 1),
+                        LocalDate.of(2024, 12, 15),
+                        AttendanceMethod.ONLINE,
+                        Set.of(educator.getId()),
+                        Set.of(student.getId())),
+                new CourseDTO(
+                        null, // id is optional and will be auto-generated
+                        "Data Structures",
+                        "A comprehensive course on data structures.",
+                        educator.getId(), // institutionId
+                        LocalDate.of(2024, 10, 1),
+                        LocalDate.of(2024, 11, 30),
+                        AttendanceMethod.IN_PERSON,
+                        Set.of(educator.getId()),
+                        Set.of(student.getId()))
+        };
+
+        for (CourseDTO courseDTO : courses) {
+            try {
+                CourseDTO createdCourse = courseService.createCourse(courseDTO);
+                logger.info("Course created: " + createdCourse);
+            } catch (Exception e) {
+                logger.error("Error creating course: ", e);
+            }
+        }
     }
 
     private void createDummyTransactions() {
