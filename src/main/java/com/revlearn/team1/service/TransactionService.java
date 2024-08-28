3b8329fd478b71b3c61f357d14b35758b338f781
Trey Crossley <treycrossley@gmail.com>
2024-08-28 09:41:51 -0700

    update tests!

--- src/main/java/com/revlearn/team1/service/TransactionService.java
+++ src/main/java/com/revlearn/team1/service/TransactionService.java
@@ -1,16 +1,18 @@
 package com.revlearn.team1.service;
 
-import com.revlearn.team1.dto.TransactionRequestDTO;
-import com.revlearn.team1.dto.TransactionResponseDTO;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.revlearn.team1.dto.TransactionRequestDTO;
+import com.revlearn.team1.dto.TransactionResponseDTO;
 
 @Service
 public interface TransactionService
 {
     public TransactionResponseDTO createTransaction(TransactionRequestDTO transaction);
     public TransactionResponseDTO getTransactionById(int transactionId);
-    List<TransactionDTO> getTransactions();
+
+    List<TransactionResponseDTO> getTransactions();
     void deleteTransactionById(int transactionId);
 }
