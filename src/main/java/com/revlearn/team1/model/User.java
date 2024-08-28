b3f9313fa71d1b4e47c8d37378e315e7eec31922
Trey Crossley <treycrossley@gmail.com>
2024-08-28 01:42:02 -0700

    added username field

--- src/main/java/com/revlearn/team1/model/User.java
+++ src/main/java/com/revlearn/team1/model/User.java
@@ -1,18 +1,28 @@
 package com.revlearn.team1.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "\"user\"") // user is a reserved keyword in H2 testing db, needs quotes to properly parse
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    private String username;
 
 }
