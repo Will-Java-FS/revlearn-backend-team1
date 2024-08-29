0d23665d50adb94b7f58db5ac1cc73e6392b59a5
Trey Crossley <treycrossley@gmail.com>
2024-08-28 21:59:50 -0700

    basic User endpoint func

--- src/main/java/com/revlearn/team1/model/User.java
+++ src/main/java/com/revlearn/team1/model/User.java
@@ -1,24 +1,36 @@
 package com.revlearn.team1.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @Table(name = "\"user\"") // user is a reserved keyword in H2 testing db, needs quotes to properly parse
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    private String username;
 
     @ManyToMany(mappedBy = "educators")
     @JsonIgnore
--- src/main/java/com/revlearn/team1/repository/UserRepository.java
+++ src/main/java/com/revlearn/team1/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.revlearn.team1.repository;
 
-import com.revlearn.team1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User,Long>{
+import com.revlearn.team1.model.User;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
