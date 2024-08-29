9fac06a7469b81c30cfc94b659ca40f9b987c2e5
Michael Nugent <43160669+nugentmichael1@users.noreply.github.com>
2024-08-29 08:15:39 -0700

    Feat/transaction course field (#25)
    * bugfix: match test routes
    
    * refactor: make course the owner of courseContent
    feat: create relationship between course and transaction
    
    * bugfix

--- src/main/java/com/revlearn/team1/model/TransactionModel.java
+++ src/main/java/com/revlearn/team1/model/TransactionModel.java
@@ -1,17 +1,7 @@
 package com.revlearn.team1.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Data
@@ -19,20 +9,23 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
-public class TransactionModel
-{
+public class TransactionModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id", referencedColumnName = "to_id")
-    User to_user;
+    User to_user; //this should be an institution (unless it is a refund)
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id", referencedColumnName = "from_id")
-    User from_user;
+    User from_user; //this should be a student (unless it is a refund)
 
     float price;
     String description;
-}
+
+    //What was purchased
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;}
