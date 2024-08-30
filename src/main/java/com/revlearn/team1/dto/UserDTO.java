163fa80d818cf1aa0c37ba1aeb815ae653770fcb
ZBDGIT <160475392+ZBDGIT@users.noreply.github.com>
2024-08-30 16:22:27 -0700

    Add the user entity to the main (#26)
    * add some User functionalities
    
    * ready for test
    
    * Unit Test
    
    * Unit Test Finished
    
    * add .gitignore file
    
    * merged successful
    
    * magic
    
    * resolved merge conflicts
    
    * add application.properties file back into git cache
    
    * cleanup: remove user exclusive stories from course
    
    * bugfix: corrected routes, relocated application-test.properties
    
    * cleanup: auto remove unused imports and extra lines
    
    ---------
    
    Co-authored-by: Michael Nugent <nugentmichael@live.com>

---
+++ src/main/java/com/revlearn/team1/dto/UserDTO.java
@@ -0,0 +1,42 @@
+package com.revlearn.team1.dto;
+
+public class UserDTO {
+
+    private String username;
+
+    private String password;
+
+    // Default constructor
+    public UserDTO() {
+    }
+
+    // Parameterized constructor
+    public UserDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginRequest{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
