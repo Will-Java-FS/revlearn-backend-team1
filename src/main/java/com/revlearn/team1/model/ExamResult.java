eff5f5208b4f2e44a900127dfc991cb689a6d2bd
ZBDGIT <160475392+ZBDGIT@users.noreply.github.com>
2024-09-04 09:52:00 -0700

    Add some Exam result entity functionalities (#52)
    * exam result entity
    
    * some exam result functions
    
    * exam result test done
    
    ---------
    
    Co-authored-by: Michael Nugent <nugentmichael@live.com>

---
+++ src/main/java/com/revlearn/team1/model/ExamResult.java
@@ -0,0 +1,34 @@
+package com.revlearn.team1.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExamResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "grade")
+    private double grade;
+
+    @CreationTimestamp
+    @Column(name = "exam_date", updatable = false, nullable = false)
+    private LocalDateTime examDate;
+
+
+    private long examId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+}
