9fa7c475db2b32a2cacd9461f687a2d417b6d37c
Trey Crossley <treycrossley@gmail.com>
2024-09-04 13:22:27 -0700

    connSuccesfful check and apparently youre not supposed to printstacktrace idk why tho

--- src/test/java/com/revlearn/team1/unit/testJdbcConnection.java
+++ src/test/java/com/revlearn/team1/unit/testJdbcConnection.java
@@ -4,16 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class testJdbcConnection {
+    private static final Logger logger = LoggerFactory.getLogger(testJdbcConnection.class);
     public static void main(String[] args) {
         String url = "${SPRING_DATASOURCE_URL}";
         String user = "${POSTGRES_USER}";
         String password = "${POSTGRES_PASSWORD}";
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            System.out.println("Connection successful!");
+            if (conn != null && conn.isValid(2)) {
+                logger.info("Connection successful!");
+                return;
+            }
+            logger.info("Connection failed or is not valid.");
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("Connection failed: " + e.getMessage(), e);
         }
     }
 }
