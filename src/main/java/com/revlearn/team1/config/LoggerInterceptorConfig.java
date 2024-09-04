3473ed6cdc0932ea5702f1a9c5efb7c5f093f9b2
Daniel Thompson <DThompson124@my.gcu.edu>
2024-09-04 11:29:28 -0700

    Added Logging via Interceptors for the application

---
+++ src/main/java/com/revlearn/team1/config/LoggerInterceptorConfig.java
@@ -0,0 +1,21 @@
+package com.revlearn.team1.config;
+
+import com.revlearn.team1.logging.LoggerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class LoggerInterceptorConfig implements WebMvcConfigurer
+{
+    @Autowired
+    LoggerInterceptor loggerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry)
+    {
+        // Makes it so that the loggerInterceptor is used for all requests (URI's)
+        registry.addInterceptor(loggerInterceptor);
+    }
+}
