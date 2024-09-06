977dd99e03f711373ffc92d0e781c69c281852e9
Michael Nugent <nugentmichael@live.com>
2024-09-05 18:09:12 -0700

    Data initializer for authorized requests

--- src/main/java/com/revlearn/team1/initializer/DataInitializer.java
+++ src/main/java/com/revlearn/team1/initializer/DataInitializer.java
@@ -9,9 +9,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -66,9 +64,17 @@ public class DataInitializer implements ApplicationRunner {
     }
 
     private void createInitialCourses(JsonNode coursesNode) {
+        JsonNode jwtNode = null;
+        //TODO: Set authorization header with JWT of educator or admin user
+        try{
+            jwtNode = getAuthorizationHeader();
+        }catch (Exception e){
+            logger.error("Exception occurred while sending request: ", e);
+        }
         String requestUrl = apiUrl + "/course";
         for (JsonNode courseNode : coursesNode) {
-            sendRequest(requestUrl, HttpMethod.POST, courseNode);
+            assert jwtNode != null;
+            sendRequestAdmin(requestUrl, HttpMethod.POST, courseNode, jwtNode.get("JWT").asText());
         }
     }
 
@@ -102,6 +108,43 @@ public class DataInitializer implements ApplicationRunner {
             }
         } catch (Exception e) {
             logger.error("Exception occurred while sending request: ", e);
+        }
+    }
+    private void sendRequestAdmin(String url, HttpMethod method, JsonNode requestBody, String jwt) {
+        try {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+            httpHeaders.setBearerAuth(jwt);
+
+            HttpEntity<JsonNode> requestEntity = new HttpEntity<>(requestBody, httpHeaders);
+            logger.info("Sending request to URL: " + url);
+            logger.info("Request body: " + requestBody.toPrettyString());
+
+            ResponseEntity<JsonNode> response = restTemplate.exchange(url, method, requestEntity, JsonNode.class);
+
+            if (response.getStatusCode().is2xxSuccessful())  {
+                logger.info("Request successful: " + response.getBody());
+            } else {
+                logger.error(
+                        "Error in request: " + response.getStatusCode() + ", Response body: " + response.getBody());
+            }
+        } catch (Exception e) {
+            logger.error("Exception occurred while sending request: ", e);
+        }
+    }
+
+    private JsonNode getAuthorizationHeader() throws IOException {
+        String requestUrl = apiUrl + "/user/register";
+        HttpMethod method = HttpMethod.POST;
+        JsonNode rootNode = objectMapper.readTree(new ClassPathResource("initial-data.json").getInputStream());
+        JsonNode requestBody = rootNode.path("adminUser");
+        try{
+            HttpEntity<JsonNode> requestEntity = new HttpEntity<>(requestBody);
+            ResponseEntity<JsonNode> response = restTemplate.exchange(requestUrl, method, requestEntity, JsonNode.class);
+            return response.getBody();
+        } catch (Exception e) {
+            logger.error("Exception occurred while sending request: ", e);
+            return null;
         }
     }
 
--- src/main/java/com/revlearn/team1/service/securityContext/SecurityContextService.java
+++ src/main/java/com/revlearn/team1/service/securityContext/SecurityContextService.java
@@ -1,5 +1,6 @@
 package com.revlearn.team1.service.securityContext;
 
+import com.revlearn.team1.enums.Roles;
 import com.revlearn.team1.model.User;
 import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.security.core.Authentication;
@@ -17,18 +18,18 @@ public interface SecurityContextService {
             if (principal instanceof User user) {
                 return (long) user.getId();
             } else
-                throw new AuthorizationServiceException("User is authenticated with object other than UserSecurityDTO.");
+                throw new AuthorizationServiceException("User is authenticated with object other than User.  (Probably did not send a JWT in request).");
         } else throw new AuthorizationServiceException("Failed to acquire user authentication information.");
     }
 
-    static String getUserRole() {
+    static Roles getUserRole() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null && authentication.isAuthenticated()) {
             Object principal = authentication.getPrincipal();
             if (principal instanceof User user) {
                 return user.getRole();
             } else
-                throw new AuthorizationServiceException("User is authenticated with object other than UserSecurityDTO.");
+                throw new AuthorizationServiceException("User is authenticated with object other than User.  (Probably did not send a JWT in request).");
         } else throw new AuthorizationServiceException("Failed to acquire user authentication information.");
     }
 }
