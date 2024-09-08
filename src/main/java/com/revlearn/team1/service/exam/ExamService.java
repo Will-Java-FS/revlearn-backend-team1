055da810a93fbb0cf7294ee2576c4d9bae065ae7
Michael Nugent <43160669+nugentmichael1@users.noreply.github.com>
2024-09-07 21:30:47 -0700

    connect module id to exam creation (#75)

--- src/main/java/com/revlearn/team1/service/exam/ExamService.java
+++ src/main/java/com/revlearn/team1/service/exam/ExamService.java
@@ -5,7 +5,7 @@ import com.revlearn.team1.model.Exam;
 public interface ExamService {
     Exam getById(Long examId);
 
-    Exam createExam(Exam exam);
+    Exam createExam(Long moduleId, Exam exam);
 
     Exam updateExam(Long examId, Exam exam);
 
