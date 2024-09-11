52cc6504aaee960707bb55f02e89b6f14afa470b
Michael Nugent <43160669+nugentmichael1@users.noreply.github.com>
2024-09-11 08:33:51 -0700

    add questions at exam creation (#107)
    * add questions at exam creation
    
    * add questions to response dto

--- src/main/java/com/revlearn/team1/dto/exam/ExamReqDTO.java
+++ src/main/java/com/revlearn/team1/dto/exam/ExamReqDTO.java
@@ -1,10 +1,15 @@
 package com.revlearn.team1.dto.exam;
 
+import com.revlearn.team1.dto.examquestion.ExamQuestionReqDTO;
+
+import java.util.List;
+
 public record ExamReqDTO(
     String title,
     String description,
     String instructions,
     Long duration,
-    String type
+    String type,
+    List<ExamQuestionReqDTO> questions
 ) {
 }
