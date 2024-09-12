4761496a8376059d564dff426eb50bc0c111b7c7
Trey Crossley <treycrossley@gmail.com>
2024-09-12 08:18:03 -0700

    fixed the app!

--- src/main/java/com/revlearn/team1/Team1Application.java
+++ src/main/java/com/revlearn/team1/Team1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Team1Application {
     public static void main(String[] args) {
+        throw new Exception("Can we pls merge this in before demo. its urgent");
         SpringApplication.run(Team1Application.class, args);
     }
 }
